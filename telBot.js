eea94e9b894e9433be0467f4942014a8767e765f
rau eyal <raueyal@gmail.com>
2021-02-16 15:43:59 +0200

    verbose debugging

--- telBot.js
+++ telBot.js
@@ -10,11 +10,17 @@ const bot = new TelegramBot(token, { polling: true });
 
 router.post('/', async (req, res) => {
   try {
-    bot.sendMessage(chatId, req.body.message);
     console.log(req.body.message);
+    bot.sendMessage(
+      chatId,
+      `DEBUG: printing message: ${JSON.stringify(req.body)}`
+    );
+    bot.sendMessage(chatId, req.body.message);
+
     return res.status(200).json(req.body.message);
   } catch (err) {
     console.error(err.message);
+    bot.sendMessage(chatId, `ERROR: ${err.message}`);
     res.status(500).send('Server error');
   }
 });
