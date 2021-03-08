2510dec768d3f2efac078bfd1cbccf6ef9e64463
rau eyal <raueyal@gmail.com>
2021-03-08 11:29:18 +0200

    env

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const db = process.env.mongoURI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.log(err.message);
+    //Exit
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
