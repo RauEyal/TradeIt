f8ef9347fc3bffa841b0506d4ba532c8133c35e3
rau eyal <raueyal@gmail.com>
2021-06-06 21:56:55 +0300

    axios and token request

--- telBot.js
+++ telBot.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const router = express.Router();
@@ -5,7 +6,11 @@ const Order = require('./models/Order');
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '1623319416:AAFPB5DMCp6wKarJj05c9rnjjQTjWP_fxmk';
 const chatId = '-591780130';
-const actions = ['SELL', 'STRONG-SELL', 'BUY', 'STRONG-BUY'];
+const actions = ['BUY', 'STRONG-BUY', 'SELL', 'STRONG-SELL'];
+
+secret = process.env.SECRET;
+client_id = process.env.API_KEY;
+refreshToken = process.env.REFRESH_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
@@ -17,8 +22,13 @@ router.post('/', async (req, res) => {
     var actionArray = JSON.parse('[' + data.action + ']');
     console.log(`symbol: ${symbol}, action:${actionArray}`);
     var actionType = actions[GetActionNumber(actionArray)];
+    const tokenResp = await GetAccessToken();
+    const token = tokenResp.access_token;
 
-    bot.sendMessage(chatId, `${symbol} - ${actionType} = ${data.price}$`);
+    bot.sendMessage(
+      chatId,
+      `${symbol} - ${actionType} = ${data.price}$ \n ${token}`
+    );
 
     let orderData = new Order({
       symbol: symbol,
@@ -38,7 +48,7 @@ router.post('/', async (req, res) => {
 router.post('/anyalert', async (req, res) => {
   try {
     console.log(`anyAlert: ${JSON.stringify(req.body)}`);
-    bot.sendMessage(chatId, `anyAlert: ${JSON.stringify(req.body)}`);
+    bot.sendMessage(chatId, req.body.message);
 
     return res.status(200).json('any alert message');
   } catch (err) {
@@ -59,24 +69,26 @@ function GetActionNumber(actionArray) {
   return -1;
 }
 
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//     // 'msg' is the received Message from Telegram
-//     // 'match' is the result of executing the regexp above on the text content
-//     // of the message
+async function GetAccessToken() {
+  const data = {
+    grant_type: 'refresh_token',
+    client_id: API_KEY,
+    refresh_token: REFRESH_TOKEN,
+    response_type: 'token',
+    client_secret: SECRET,
+  };
 
-//     const chatId = msg.chat.id;
-//     const resp = match[1]; // the captured "whatever"
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
 
-//     // send back the matched "whatever" to the chat
-//     bot.sendMessage(chatId, resp);
-//   });
+  return axios.post(
+    'https://api.tradestation.com/v2/Security/Authorize',
+    data,
+    {
+      headers,
+    }
+  );
+}
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id;
-
-//   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(chatId, 'Received your message');
-// });
 module.exports = router;
